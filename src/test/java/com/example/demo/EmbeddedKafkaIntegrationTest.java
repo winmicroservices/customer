82c19f765dbf61014e03c9921c08751b828f3d85
William Polinchak <william.polinchak@windstream.com>
2023-01-11 16:38:28 -0500

    feat(Kafka): #35 Working on Kafka tests.

---
+++ src/test/java/com/example/demo/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.TopicListener;
+import com.example.demo.service.TopicProducer;
+import com.example.demo.util.Util;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+class EmbeddedKafkaIntegrationTest {
+
+  @Autowired
+  TopicProducer producer;
+
+  @Autowired
+  TopicListener consumer;
+
+  @Value("${test.topic}")
+  private String topic;
+
+  @Test
+  public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived() 
+    throws Exception {
+    Customer c = new Customer("Bill","Polinchak","Venice");
+    String json = Util.asJsonString(c);
+    producer.send(json);
+    assertTrue(true);
+  }
+}
