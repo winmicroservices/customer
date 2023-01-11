12121c718afc323d67494d9f1e9023e22f62b294
William Polinchak <william.polinchak@windstream.com>
2023-01-11 08:37:08 -0500

    fix: #35 Adding javadocs.

--- src/main/java/com/example/demo/CustomerApplication.java
+++ src/main/java/com/example/demo/CustomerApplication.java
@@ -9,6 +9,10 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
 public class CustomerApplication {
 
+	/**
+	 * This is the main method for the Springboot application.
+	 * @param args Command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
