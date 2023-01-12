c1d98089acc94de1796596d9a0b09dcaf93b3085
William Polinchak <william.polinchak@windstream.com>
2023-01-12 10:46:48 -0500

    fix: #35 Format class.

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -3,17 +3,18 @@ package com.example.demo.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
-* This class contains some utility methods.
-*/
+ * This class contains some utility methods.
+ */
 public class Util {
 
-    /**
-     * Returns a json representation of an object.
-     * @param obj The object to be converted.
-     * @return A json representation of the object.
-     * @throws Exception If something breaks.
-     */
-    public static String asJsonString(final Object obj) throws Exception {
-		return new ObjectMapper().writeValueAsString(obj);
-	}
+  /**
+   * Returns a json representation of an object.
+   * 
+   * @param obj The object to be converted.
+   * @return A json representation of the object.
+   * @throws Exception If something breaks.
+   */
+  public static String asJsonString(final Object obj) throws Exception {
+    return new ObjectMapper().writeValueAsString(obj);
+  }
 }
