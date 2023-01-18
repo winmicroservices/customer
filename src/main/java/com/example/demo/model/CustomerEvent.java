e1a8685c9ec6a822aeb6d8cb7674bf9a958bde75
William Polinchak <william.polinchak@windstream.com>
2023-01-18 10:57:37 -0500

    fix: #37 Adding the forgein key to the customer_event table.

--- src/main/java/com/example/demo/model/CustomerEvent.java
+++ src/main/java/com/example/demo/model/CustomerEvent.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Table(name = "customer_event")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -18,24 +22,38 @@ public class CustomerEvent extends Customer {
     @Enumerated(EnumType.STRING)
     private State state;
 
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "customer_fk")
+    @JsonManagedReference
+    private Customer customer;
+
+    
     public CustomerEvent() {
         
     }
-        
+    
     public CustomerEvent(State state, Customer customer) {
         this.state = state;
+        this.customer = customer;
         super.setCity(customer.getCity());
         super.setFirstName(customer.getFirstName());
         super.setLastName(customer.getLastName());
         super.setCreditLimit(customer.getCreditLimit());
     }
- 
+    
     public State getState() {
         return this.state;
     }
-
+    
     public void setState(State state) {
         this.state = state;
     }
     
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
